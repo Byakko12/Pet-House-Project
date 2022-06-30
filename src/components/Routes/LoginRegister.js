39ce7132325ed2d95edfe1a3b8a048783cbc1967
DanielCeron-Holberton <3403@holbertonschool.com>
2022-06-30 00:39:37 -0500

    [ADD] - Final dependencies for react environment

--- src/components/Routes/LoginRegister.js
+++ src/components/Routes/LoginRegister.js
@@ -1,14 +1,14 @@
-import Navbar from '../Navbar/Navbar'
-import Login from '../Login/Login';
-import Register from '../Register/Register'
+import React from "react";
+import Navbar from "../Navbar/Navbar";
+import Login from "../Login/Login";
+import Register from "../Register/Register";
 const LoginRegister = () => {
-    return(
-            <main className="login-register">
-                <Navbar/>
-                <Login/>
-                <Register/>
-            </main>
-
-    )
-}
+  return (
+    <main className="login-register">
+      <Navbar />
+      <Login />
+      <Register />
+    </main>
+  );
+};
 export default LoginRegister;
